3d8d51154f9a602776ae117642dc3c75d36e4276
Batbuyan Buyandelger <bbuyan5@gmail.com>
2026-05-03 19:03:57 +0200

    use telescope to go to definition, implementations and references

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function ()
             require("mason-lspconfig").setup({
-                ensure_installed = { "omnisharp", "lua_ls" }
+                ensure_installed = { }
             })
         end
     },
@@ -22,16 +22,18 @@ return {
 
             vim.lsp.enable("omnisharp")
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local tele = require("telescope.builtin")
 
         local on_attach = function(client, bufnr)
             local opts = { noremap = true, silent = true, buffer = bufnr }
 
             vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- Show documentation on hover
-            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts) -- Go to definition
+            vim.keymap.set("n", "<leader>gd", tele.lsp_definitions , opts) -- Go to definition
+            vim.keymap.set("n", "<leader>gi", tele.lsp_implementations, opts) -- Go to implementation
+            vim.keymap.set("n", "<leader>gr", tele.lsp_references, opts) -- Show references
+
             vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- Go to declaration
-            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts) -- Go to implementation
-            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts) -- Show references
-            vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, opts) -- Show references
+            vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, opts) -- Format the text
             vim.keymap.set('n', '<space>e', function()
                 vim.diagnostic.open_float(0, { scope = "line" })
             end, { noremap = true, silent = true, buffer = bufnr })
@@ -42,25 +44,5 @@ return {
           on_attach = on_attach,
         })
         end
-        --   local capabilities = require('cmp_nvim_lsp').default_capabilities()
-        --
-        --   local lspconfig = require("lspconfig")
-        --
-
-
-        --   lspconfig.omnisharp.setup({
-        --     cmd={"omnisharp"},
-        --     capabilities = capabilities,
-        --     on_attach = on_attach,
-        --     FormattingOptions = {
-        --       EnableEditorConfigSupport = true,
-        --       OrganizeImprts = true,
-        --     }
-        --   })
-        --
-        --   lspconfig.pyright.setup({
-        --     capabilities = capabilities,
-        --     on_attach = on_attach
-        --   })
     }
 }
