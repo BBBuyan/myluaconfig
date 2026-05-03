9263a932598a9b64cc8322db266be4e594f82427
Batbuyan Buyandelger <bbuyan5@gmail.com>
2026-05-03 18:43:46 +0200

    on_attach keybindings set

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -14,6 +14,7 @@ return {
     config = function ()
       local cmp = require'cmp'
       local luasnip = require("luasnip")
+
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -27,7 +28,7 @@ return {
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-c>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
           ["<Tab>"] = cmp.mapping(function(fallback)
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -21,24 +21,31 @@ return {
             vim.lsp.enable("lua_ls")
 
             vim.lsp.enable("omnisharp")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+        local on_attach = function(client, bufnr)
+            local opts = { noremap = true, silent = true, buffer = bufnr }
+
+            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- Show documentation on hover
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts) -- Go to definition
+            vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- Go to declaration
+            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts) -- Go to implementation
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts) -- Show references
+            vim.keymap.set("n", "<leader>fo", vim.lsp.buf.format, opts) -- Show references
+            vim.keymap.set('n', '<space>e', function()
+                vim.diagnostic.open_float(0, { scope = "line" })
+            end, { noremap = true, silent = true, buffer = bufnr })
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
+        end
+        vim.lsp.config("*", {
+          capabilities = capabilities,
+          on_attach = on_attach,
+        })
         end
         --   local capabilities = require('cmp_nvim_lsp').default_capabilities()
         --
         --   local lspconfig = require("lspconfig")
         --
-        --   local on_attach = function(client, bufnr)
-        --     local opts = { noremap = true, silent = true, buffer = bufnr }
-        --
-        --     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts) -- Show documentation on hover
-        --     vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts) -- Go to definition
-        --     vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts) -- Go to declaration
-        --     vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts) -- Go to implementation
-        --     vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts) -- Show references
-        --     vim.keymap.set('n', '<space>e', function()
-        --       vim.diagnostic.open_float(0, { scope = "line" })
-        --     end, { noremap = true, silent = true, buffer = bufnr })
-        --     vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "LSP Code Action" })
-        --   end
 
 
         --   lspconfig.omnisharp.setup({
