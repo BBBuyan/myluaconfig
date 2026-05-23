252ca37745950d74b86a37c280935868404d70b9
Batbuyan Buyandelger <bbuyan5@gmail.com>
2026-05-23 21:57:44 +0200

    completions cleared

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -24,12 +24,8 @@ return {
                     end,
                 },
                 window = {
-                    completion = cmp.config.window.bordered({
-                        border = "single"
-                    }),
-                    documentation = cmp.config.window.bordered({
-                        border = "single"
-                    }),
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
                     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
