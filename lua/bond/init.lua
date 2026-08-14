393928c68b6f21ad41907a1dd5af844edaf65afa
Batbuyan Buyandelger <batbuyan.buyandelger@stud.htwk-leipzig.de>
2026-08-14 19:19:55 +0200

    comment-out windows configs

--- lua/bond/init.lua
+++ lua/bond/init.lua
@@ -1,9 +1,9 @@
 require("bond.remap")
 require("bond.format")
-vim.opt.shell = "pwsh"
-vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
-vim.opt.shellxquote = ""
-vim.opt.termguicolors = true
+-- vim.opt.shell = "pwsh"
+-- vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+-- vim.opt.shellxquote = ""
+-- vim.opt.termguicolors = true
 
 vim.filetype.add({
     extension = {
