a8fc754caf570a7ecf7905529bd931d12eff8a3c
Batbuyan Buyandelger <bbuyan5@gmail.com>
2026-08-17 14:43:03 +0200

    windows shell in if statement

--- lua/bond/windows_shell.lua
+++ lua/bond/windows_shell.lua
@@ -1,4 +1,8 @@
-vim.opt.shell = "pwsh"
-vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
-vim.opt.shellxquote = ""
-vim.opt.termguicolors = true
+local os_name = vim.uv.os_uname().sysname;
+
+if os_name == "Windows_NT" then
+    vim.opt.shell = "pwsh"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command"
+    vim.opt.shellxquote = ""
+    vim.opt.termguicolors = true
+end
